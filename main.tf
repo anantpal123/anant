4cbe9172a2c5b5718933b0d4ff6f28ed3ec57332
Anant Pal <anantpal9927@gmail.com>
2025-07-07 11:15:03 +0530

    subscrtion

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 terraform {
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
-        }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.35.0"
     }
-    
-    required_version = ">= 1.0"
+  }
 }
+
+provider "azurerm" {
+    features { }
+      subscription_id = "9c2d44b8-0b58-4481-8de2-41223a92f641"
+      }
